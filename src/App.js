7f5f7476e34c63c15632e637ea06d7862325beac
Luka Pavicic <lukapavicic01@gmail.com>
2019-09-20 23:37:28 +0200

    Development PAUSED

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import Navbar from './components/Navbar';
 import LoginScreen from './screens/Auth/LoginScreen';
 import RegisterScreen from './screens/Auth/RegisterScreen';
 
-function App() {
-  return (
-    <Router>
-      <div style={{width: "100%", height: "100vh"}}>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/" component={HomeScreen}/>
-          <Route exact path="/login" component={LoginScreen}/>
-          <Route exact path="/register" component={RegisterScreen}/>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div style={{width: "100%", height: "100vh"}}>
+          <Navbar/>
+          <Switch>
+            <Route exact path="/" component={HomeScreen}/>
+            <Route exact path="/login" component={LoginScreen}/>
+            <Route exact path="/register" component={RegisterScreen}/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,7 +6,8 @@ export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [{'title': "ETS2 is epic", 'content': 'Eiusmod dolor cillum aliquip do. Commodo elit et ea ea occaecat occaecat incididunt enim deserunt cillum non exercitation sint. Velit laboris quis aliqua culpa consectetur dolore cillum laborum. Amet eiusmod enim ullamco est in fugiat do dolore adipisicing culpa mollit consectetur ullamco. Incididunt sit nostrud dolor tempor magna elit. Deserunt deserunt aliqua magna ea anim fugiat deserunt nulla consequat.'},1,1,1,1,1,1],
+      posts: [],
+      userAuthenticated: false,
       user: {
         joined_communities: [
           {id: 1, name: 'Programming'},
@@ -15,43 +16,62 @@ export default class HomeScreen extends React.Component {
           {id: 76, name: 'DankMemes'},
           {id: 33, name: 'Django'},
         ]
-      }
+      },
+      top_communities: [{'id': 1, 'name': 'WebDevelopment'}, {'id': 23, 'name': 'Memes'}]
     }
   }
 
   render() {
-    if(this.state.posts.length === 0) {
-      return (
-        <div className="container">
-          <img src="/undraw_post2_19cj.svg" alt="posts" className="posts-svg" />
-          <h2>There are no posts to show.</h2>
-        </div>
-      )
-    } else {
-      return (
-        <div className="homescreen-wrapper">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-8 col-md-8 col-xs-12">
-                <h5>Posts</h5>
-                {this.state.posts.map(post => (
-                  <div className="single-post-wrapper">
-
-                  </div>
-                ))}
-              </div>
-              <div className="col-lg-4 col-md-4 col-xs-0 position-sticky">                
-                <h5>Joined communities</h5>
-                <ul className="list-group">
-                  {this.state.user.joined_communities.map(community => (
-                    <li className="list-group-item">{community.name}</li>
+      if(this.state.userAuthenticated) {
+        return (
+          <div className="homescreen-wrapper">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-8 col-md-8 col-xs-12">
+                  <h5>Posts</h5>
+                  {this.state.posts.map(post => (
+                    <div key={post.id} className="single-post-wrapper">
+  
+                    </div>
                   ))}
-                </ul>
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-0 position-sticky">                
+                  <h5>Joined communities</h5>
+                  <ul className="list-group">
+                    {this.state.user.joined_communities.map((community) => (
+                      <li key={community.id} className="list-group-item">{community.name}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>          
+            </div>
+          </div>        
+        )
+      } else {
+        return (
+          <div className="homescreen-wrapper">
+            <div className="container">
+            <div className="row">
+                <div className="col-lg-8 col-md-8 col-xs-12">
+                  <h5>Recent Posts</h5>
+                  {this.state.posts.map(post => (
+                    <div key={post.id} className="single-post-wrapper">
+  
+                    </div>
+                  ))}
+                </div>
+                <div className="col-lg-4 col-md-4 col-xs-0 position-sticky">                
+                  <h5>Popular Communities</h5>
+                  <ul className="list-group">
+                    {this.state.user.joined_communities.map((community) => (
+                      <li key={community.id} className="list-group-item">{community.name}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
-            </div>          
+            </div>
           </div>
-        </div>        
-      )
-    }
+        )
+      }       
   }
 }
